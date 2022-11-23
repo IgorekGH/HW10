0224b933c970b7acf632274fa23f876e9c600844
Igorek <igorek89nur@mail.ru>
2022-11-23 13:34:29 +0700

    DZ

---
+++ zadacha2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+if(a>b){Console.WriteLine("Число "+ a +" больше числа " + b);}
+else{Console.WriteLine("Число " + b + " больше числа " + a);}
---
+++ zadacha6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Напишите число");
+int a=Convert.ToInt32(Console.ReadLine());
+if(a%2==1){Console.WriteLine(+a + " нечетное");}
+else{Console.WriteLine (+a + " четное");}
